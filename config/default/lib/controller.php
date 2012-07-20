0ef0621397c5b53251cacbb17a7f60e14b72d1f6
Matthew J. Sahagian <msahagian@dotink.org>
2012-07-20 11:59:04 -0700

    Fixed autoloading for controllers.

--- config/default/lib/controller.php
+++ config/default/lib/controller.php
@@ -2,11 +2,6 @@
 
 	return iw::createConfig('Library', array(
 
-		// Whether or not we should attempt to autoload classes which match this class from the
-		// root_directory
-
-		'auto_load' => TRUE,
-
 		// Whether or not we should attempt to auto scaffold records using this class.
 
 		'auto_scaffold' => FALSE,
@@ -14,6 +9,11 @@
 		// The directory relative to application root in which user defined record sets are stored.
 
 		'root_directory' => 'user/controllers',
+
+		// The wildcard autoloader means to use this classes __match() method and attemp to load
+		// from it's root directory.  Removing it will remove controller autoloading.
+
+		'autoloaders' => array('*'),
 
 		// The default accept types in preferred order.
 
